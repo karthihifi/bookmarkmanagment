b754c24a4ae5898447dbb74f3e341fdd50fce2c3
karthihifi <karthi.hifi@gmail.com>
2021-11-13 22:19:20 +0530

    commit

--- src/Components/FileViewEdit.js
+++ src/Components/FileViewEdit.js
@@ -7,7 +7,7 @@ import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import { EditorState, ContentState, convertToRaw } from "draft-js";
 import draftToHtml from "draftjs-to-html";
-import "./FileViewEdit.css"
+import "./FileViewEdit.css";
 // import htmlToDraft from 'html-to-draftjs';
 
 const FileViewEdit = (props) => {
@@ -29,59 +29,73 @@ const FileViewEdit = (props) => {
     <div className="FileViewEdit">
       <NavBarRootView view="FileViewEdit" />
       <div className="FileViewEdit-container">
-      <Form>
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
-          <Form.Label column sm={2}>
-            Title
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              type="email"
-              placeholder="Email"
-              value={state.title}
-            />
-          </Col>
-        </Form.Group>
+        <h3 className="FileViewEdit-header">{state.title}</h3>
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Title
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                value={state.title}
+              />
+            </Col>
+          </Form.Group>
 
-        <Form.Group
-          as={Row}
-          className="mb-3"
-          controlId="formHorizontalPassword"
-        >
-          <Form.Label column sm={2}>
-            Category
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control
-              type="text"
-              placeholder="Password"
-              value={state.category}
-            />
-          </Col>
-        </Form.Group>
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formHorizontalPassword"
+          >
+            <Form.Label column sm={2}>
+              Category
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type="text"
+                placeholder="Password"
+                value={state.category}
+              />
+            </Col>
+          </Form.Group>
 
-        <Form.Group
-          as={Row}
-          className="mb-3"
-          controlId="formHorizontalPassword"
-        >
-          <Form.Label column sm={2}>
-            Comments
-          </Form.Label>
-          <Col sm={10} className="Wysig_editor">
-            {/* <Form.Control type="text" placeholder="Password" value={state.category}/> */}
-            <Editor 
-              defaultContentState={DefaultState}
-              onContentStateChange={setDefaultState}
-              //   editorState={editorState}
-              wrapperClassName="wrapper-class"
-              editorClassName="editor-class"
-              toolbarClassName="toolbar-class"
-              onEditorStateChange={onEditorStateChange}
-            />
-          </Col>
-        </Form.Group>
-      </Form>
+          <Form.Group as={Row} className="mb-3" controlId="formHorizontalEmail">
+            <Form.Label column sm={2}>
+              Image
+            </Form.Label>
+            <Col sm={10}>
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                value={state.imageurl}
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group
+            as={Row}
+            className="mb-3"
+            controlId="formHorizontalPassword"
+          >
+            <Form.Label column sm={2}>
+              Comments
+            </Form.Label>
+            <Col sm={10} className="Wysig_editor">
+              {/* <Form.Control type="text" placeholder="Password" value={state.category}/> */}
+              <Editor
+                defaultContentState={DefaultState}
+                onContentStateChange={setDefaultState}
+                //   editorState={editorState}
+                wrapperClassName="wrapper-class"
+                editorClassName="editor-class"
+                toolbarClassName="toolbar-class"
+                onEditorStateChange={onEditorStateChange}
+              />
+            </Col>
+          </Form.Group>
+        </Form>
       </div>
     </div>
   );
