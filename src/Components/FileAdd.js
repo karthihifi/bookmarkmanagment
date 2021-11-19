123af7707576b999466203c7746c380a529d926d
karthihifi <karthi.hifi@gmail.com>
2021-11-19 07:02:42 +0000

    karthik-bas commit3

--- src/Components/FileAdd.js
+++ src/Components/FileAdd.js
@@ -29,6 +29,7 @@ import "./FileAdd.css";
 const FileAdd = () => {
   const [editorState, setContentState] = useState(EditorState.createEmpty());
   const [inputUrlList, setUrlList] = useState([{ title: "", url: "" }]);
+  const [inputTagList, setTagList] = useState([""]);
 
   const onEditorStateChange = (editorState) => {
     setContentState(editorState);
@@ -43,6 +44,16 @@ const FileAdd = () => {
     const list = [...inputUrlList];
     list.splice(index, 1);
     setUrlList(list);
+  };
+
+  const addTagRow = () => {
+    setTagList([...inputTagList, ""]);
+  };
+
+  const removeTagRow = (index) => {
+    const list = [...inputTagList];
+    list.splice(index, 1);
+    setTagList(list);
   };
 
   function navbar() {
@@ -69,6 +80,8 @@ const FileAdd = () => {
     );
   }
 
+  const errortit = true;
+
   return (
     <div className="FileAdd">
       <FileAddNavbar />
@@ -81,8 +94,9 @@ const FileAdd = () => {
         autoComplete="on"
       >
         <div className="FileAdd-Container">
-          <div>
-            <FormControl variant="standard" required>
+          <section>
+            <h4 className="FileAdd-header">Main Section</h4>
+            <FormControl error = {!errortit} variant="standard" required sx={{ mr: 4 }}>
               <InputLabel htmlFor="my-input">Article Title</InputLabel>
               <Input
                 id="my-input"
@@ -117,9 +131,9 @@ const FileAdd = () => {
                 Enter Cover Image.
               </FormHelperText>
             </FormControl>
-          </div>
+          </section>
 
-          <FormControl variant="standard" sx={{ width: "100ch" }}>
+          <FormControl variant="standard" sx={{ width: "100ch", mb: 4 }}>
             <InputLabel htmlFor="DraftEditor">Comments</InputLabel>
             {/* <div>Comments</div> */}
             <div className="FileAdd-commentEditor">
@@ -133,48 +147,81 @@ const FileAdd = () => {
               />
             </div>
           </FormControl>
+          <section className="FileAdd-refsec">
+            <h4 className="FileAdd-header">Reference Section</h4>
+            {inputUrlList.map((link, index) => (
+              <div className="FileAdd-Reflink">
+                <Stack direction="row" spacing={2} sx={{ width: "100ch" }}>
+                  <FormControl variant="standard" sx={{ width: "40ch" }}>
+                    <InputLabel htmlFor="my-input">Article Title</InputLabel>
+                    <Input
+                      id="my-input"
+                      aria-describedby="my-helper-text"
+                      placeholder="Reference Title"
+                    />
+                    <FormHelperText id="my-helper-text">
+                      Enter Ref. Title.
+                    </FormHelperText>
+                  </FormControl>
 
-          {inputUrlList.map((link, index) => (
-            <div className="FileAdd-Reflink">
+                  <FormControl variant="standard" sx={{ width: "75ch" }}>
+                    <InputLabel htmlFor="my-input">Reference Link</InputLabel>
+                    <Input
+                      id="my-input"
+                      aria-describedby="my-helper-text"
+                      placeholder="Reference Title"
+                    />
+                    <FormHelperText id="my-helper-text">
+                      Enter Ref. Link.
+                    </FormHelperText>
+                  </FormControl>
+                  <Stack direction="row" spacing={2} sx={{ width: "50ch" }}>
+                    <Button size="small" onClick={() => addRow(index)}>
+                      Add Row
+                    </Button>
+                    {index == 0 ? (
+                      ""
+                    ) : (
+                      <Button size="small" onClick={() => removeRow(index)}>
+                        Delete Row
+                      </Button>
+                    )}
+                  </Stack>
+                </Stack>
+              </div>
+            ))}
+          </section>
+          <section className="FileAdd-tagsec">
+            <h4>Tags Section</h4>
+            {inputTagList.map((tag, index) => (
               <Stack direction="row" spacing={2} sx={{ width: "100ch" }}>
-                <FormControl variant="standard" sx={{ width: "40ch" }}>
-                  <InputLabel htmlFor="my-input">Article Title</InputLabel>
+                <FormControl variant="standard" sx={{ width: "15ch" }}>
+                  <InputLabel htmlFor="my-input">Tag</InputLabel>
                   <Input
                     id="my-input"
                     aria-describedby="my-helper-text"
-                    placeholder="Reference Title"
+                    placeholder="Enter Tag"
                   />
                   <FormHelperText id="my-helper-text">
-                    Enter Ref. Title.
+                    Enter Tag. Title.
                   </FormHelperText>
                 </FormControl>
 
-                <FormControl variant="standard" sx={{ width: "75ch" }}>
-                  <InputLabel htmlFor="my-input">Reference Link</InputLabel>
-                  <Input
-                    id="my-input"
-                    aria-describedby="my-helper-text"
-                    placeholder="Reference Title"
-                  />
-                  <FormHelperText id="my-helper-text">
-                    Enter Ref. Link.
-                  </FormHelperText>
-                </FormControl>
                 <Stack direction="row" spacing={2} sx={{ width: "50ch" }}>
-                  <Button size="small" onClick={() => addRow(index)}>
+                  <Button sx={{}} size="small" onClick={() => addTagRow(index)}>
                     Add Row
                   </Button>
                   {index == 0 ? (
                     ""
                   ) : (
-                    <Button size="small" onClick={() => removeRow(index)}>
+                    <Button size="small" onClick={() => removeTagRow(index)}>
                       Delete Row
                     </Button>
                   )}
                 </Stack>
               </Stack>
-            </div>
-          ))}
+            ))}
+          </section>
         </div>
       </Box>
       <AppBarBottom></AppBarBottom>
